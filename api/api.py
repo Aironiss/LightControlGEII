b26b780863530db1b5256b30f9ed003cfb39e059
Aironiss <yosa382@gmail.com>
2024-10-14 11:42:31 +0200

    api - first commit

---
+++ api/api.py
@@ -0,0 +1,22 @@
+import flask
+import json
+import random
+
+blueprint = flask.Blueprint("api", __name__, template_folder=template)
+
+@blueprint.route('/state')
+def getState():
+	print("getState called")
+	return 0
+
+
+@blueprint.route('/turn-on')
+def setStateOn(light):
+	print("setStateOn called")
+	return 0
+
+
+@blueprint.route('/turn-off')
+def setStateOff():
+	print("setStateOff called")
+	return 0
